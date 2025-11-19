7a84a8e12ee484c68ec18d2e1b38b0cb98c41eeb
Adam Harits <adamharits25@gmail.com>
2025-11-19 16:34:42 +0000

    adding official logo to the login page

--- src/components/auth-layout.tsx
+++ src/components/auth-layout.tsx
@@ -1,4 +1,5 @@
 import { Atom } from 'lucide-react';
+import Image from 'next/image';
 import React from 'react';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
@@ -7,8 +8,12 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
       <div className="w-full max-w-sm">
         <div className="flex flex-col items-center mb-6">
           <div className="flex items-center gap-2.5 text-primary">
-            <Atom size={36} strokeWidth={2.5} />
-            <h1 className="text-4xl font-medium font-headline text-foreground tracking-tight">Kivo</h1>
+            {/* <Atom size={36} strokeWidth={2.5} />
+            <h1 className="text-4xl font-medium font-headline text-foreground tracking-tight">Kivo</h1> */}
+
+            <Image width={150} height={150} src={'/logo.png'} alt="app logo">
+
+            </Image>
           </div>
         </div>
         {children}
